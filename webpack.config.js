b67f9cb894de6f4d47828761cc88e234b8c2228d
yinxueyuan <838476901@qq.com>
2017-05-12 18:47:04 +0800

    feat: 添加 webpack config

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const proxy = require('http-proxy-middleware');
+const path = require('path');
+const glob = require('glob');
+
+const files = glob.sync(__dirname + '/src/pages/*/index.js');
+let newEntries = {};
+
+files.forEach(function (file) {
+    var name = /.*\/(src\/.*?\/index)\.js/.exec(file)[1]; //得到src/actEnter/这样的文件名
+    name = name.split('src/pages/')[1].split('/index')[0];
+    newEntries[name] = file;
+});
+
+const isUseProxy = true;    //是否需要本地代理
+const config = {
+    devtool: 'eval-source-map', //开发环境使用;线上环境请禁用
+    entry: Object.assign({}, newEntries),
+    output: {
+        publicPath: '/static',
+        path: path.resolve(__dirname, './static'),
+        filename: '[name]/app.js'
+    },
+
+    devServer: {
+        stats: {                    //打包完，会提供一个 stats 对象，这里有每个 chunk 的详细信息
+          chunks: false
+        },
+        contentBase: './',          //本地服务器所加载的页面所在的目录
+        inline: false,              //设置为true，当源文件改变时会自动刷新页面
+        port: 8081,                 //设置默认监听端口，如果省略，默认为8080
+        historyApiFallback: false,  //在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        colors: true,               //设置为true，使终端输出的文件为彩色的
+        hot: true,                  //是否热部署
+        quiet: false,               //让dev server处于静默的状态启动
+        host: '0.0.0.0',            //hostname or IP. 0.0.0.0 binds to all hosts.
+        proxy: isUseProxy ? {       //是否需要本地代理
+            '/run/*': {
+                target: 'http://rr.leoao.com',
+                changeOrigin: true,
+                secure: false
+            },
+            '/wap/*': {
+                target: 'http://d.leoao.com',
+                changeOrigin: true,
+                secure: false
+            }
+        } : {}
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                //loader: 'style!css?modules!postcss'
+                loader: 'style!css!postcss'
+            },
+            {
+                test: /\.scss$/,
+                //loader: 'style!css?modules!sass!postcss'
+                loader: 'style!css!sass!postcss'
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=50000&name=[path][name].[ext]'
+            }
+        ]
+    },
+    externals: {
+        //'AMap': 'window.AMap'
+    },
+    postcss: [
+        require('autoprefixer') //调用autoprefixer插件
+    ],
+    plugins: [
+        new webpack.DefinePlugin({
+            __ONLINE__: isUseProxy       //是否使用代理(ps: isUseProxy=false, 调用本地mock接口)
+        }),
+        new OpenBrowserPlugin({url: 'http://localhost:8081/demos'}), //打包完成后自动打开浏览器
+        new webpack.HotModuleReplacementPlugin(),           //热加载插件
+        new webpack.NoErrorsPlugin()                        //允许错误不打断程序
+    ]
+};
+
+module.exports = config;
---
+++ webpack.production.config.js
@@ -0,0 +1,91 @@
+const webpack = require('webpack');
+const glob = require('glob');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const files = glob.sync(__dirname + '/src/pages/*/index.js');
+let newEntries = {};
+let plugins = [];
+files.forEach(function (file) {
+    var name = /.*\/(src\/.*?\/index)\.js/.exec(file)[1]; //得到src/actEnter/这样的文件名
+    name = name.split('src/pages/')[1].split('/index')[0];
+    newEntries[name] = file;
+    plugins.push(
+      new HtmlWebpackPlugin({                         //生成模板文件
+          filename: path.resolve(__dirname, `./dist/${name}/index.html`),
+          template: path.resolve(__dirname, `./demos/${name}.html`),
+          // hash: true,    //为静态资源生成hash值
+          minify: {    //压缩HTML文件
+              removeComments: true,    //移除HTML中的注释
+              collapseWhitespace: false    //删除空白符与换行符
+          },
+          inject: true,
+          chunks: [name]
+      })
+    )
+});
+
+const config = {
+    entry: Object.assign({}, newEntries),
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name]/app.[chunkhash].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                //loader: 'style!css?modules!postcss' //如果模块化使用
+                //loader: 'style!css!postcss'         //js和css打包成一个js文件(需注释插件ExtractTextPlugin)
+                loader: ExtractTextPlugin.extract('style', 'css!postcss') //js和css分开打包
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style', 'css!sass!postcss') //js和css分开打包
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=50000&name=[path][name].[ext]'
+            }
+        ]
+    },
+
+    postcss: [
+        require('autoprefixer') //调用autoprefixer插件
+    ],
+    plugins: plugins.concat([
+        new webpack.DefinePlugin({
+            __ONLINE__: true,
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new CleanWebpackPlugin(['dist'], {
+            'root': path.resolve(__dirname),
+            verbose: true,
+            dry: false
+        }),
+        new webpack.BannerPlugin('Copyright leke inc.'),    //打包文件抬头
+        //new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+        new webpack.optimize.UglifyJsPlugin({               //压缩JS代码
+            output: {
+                comments: false                             // remove all comments
+            },
+            compress: {
+                warnings: false                             //不显示warning
+            }
+        }),
+        new ExtractTextPlugin('[name]/app.[chunkhash].css'),           //是否分离CSS和JS文件('[name]-[hash].css')
+    ])
+};
+
+module.exports = config;
